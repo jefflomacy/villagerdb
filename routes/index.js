fbf4130f8e50b465e812d7abd387fa8a1ebf1623
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2019-12-01 13:02:51 -0500

    App state data back on home page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const appState = require('../helpers/app-state');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const data = {
     pageTitle: 'Home'
   };
 
-  res.render('index', data);
+  appState.getAppState(res)
+      .then((state) => {
+        Object.assign(data, state);
+        res.render('index', data);
+      })
+      .catch(next);
 });
 
 module.exports = router;

39ed677749c791fafaf7cbb739d9022e6638335c
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2020-02-22 16:35:34 -0500

    remove select code from redis controller

--- config/session/store.js
+++ config/session/store.js
@@ -1,11 +1,3 @@
 const redis = require('redis');
 const redisClient = redis.createClient(process.env.REDIS_SESSION_CONNECT_STRING);
-
-// Select the proper database.
-redisClient.select(1, (error) => {
-    if (error) {
-        throw new Error('Could not select database 1 for session store: ' + error);
-    }
-});
-
 module.exports = redisClient;

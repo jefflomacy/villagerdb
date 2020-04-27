de0384f820feed37881f5d22d20985bc1dabc6c7
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2020-04-27 17:38:33 -0400

    fixed up issue with striped backgrounds plus some other usability problems

--- src/list-search.js
+++ src/list-search.js
@@ -8,6 +8,13 @@
  */
 const $ = require('jquery');
 
+/**
+ * Contains all the list elements after detachment. They will be re-inserted if they apply.
+ *
+ * @type {jQuery}
+ */
+let listElements = undefined;
+
 $(document).ready(() => {
     // Search button click
     $('#user-list-search-submit').on('click', (e) => {
@@ -20,25 +27,8 @@ $(document).ready(() => {
             return;
         }
 
-        // Show/hide depending on match.
-        let itemCount = 0;
-        $('ul.user-list-view li')
-            .each((i, li) => {
-                const name = $(li).data('name').toLowerCase();
-                console.log(searchQuery + '; ' + name + '; ' + name.indexOf(searchQuery));
-                if (typeof name !== 'string' || name.indexOf(searchQuery) === -1) {
-                    $(li).attr('style', 'display: none !important;'); // some bad bootstrap practice we have to override
-                } else {
-                    $(li).attr('style', ''); // show
-                    itemCount++;
-                }
-            });
-
-        // Update count and show notice.
-        const resultText = (itemCount === 1) ? 'result' : 'results';
-        $('#user-list-size').text(itemCount + ' ' + resultText);
-        $('#user-list-search-query').text($('#user-list-search').val());
-        $('#user-list-search-notice').show();
+        // Filter
+        filterItems(searchQuery);
     });
 
     // Clear click.
@@ -52,12 +42,62 @@ $(document).ready(() => {
  * Display full list again, no more query.
  */
 function removeQuery() {
-    // Show all items.
-    $('ul.user-list-view li').attr('style', '');
+    // Clear query.
+    filterItems();
 
     // Hide notice.
     $('#user-list-search-notice').hide();
 
-    // Put count back.
-    $('#user-list-size').text($('#user-list-size').data('orig-text'));
+    // Empty textbox.
+    $('#user-list-search').val('');
+}
+
+/**
+ * Filter the list. If no query provided, just shows all.
+ *
+ * @param searchQuery
+ * @returns {number}
+ */
+function filterItems(searchQuery) {
+    let itemCount = 0;
+
+    // Did we gather up all the list elements yet? If not, do so.
+    if (!listElements) {
+        listElements = $('ul.user-list-view li').detach();
+    } else {
+        // Otherwise, detach all again.
+        $('ul.user-list-view li').detach();
+    }
+
+    // Loop through and add each back as needed.
+    for (let element of listElements) {
+        const name = $(element).data('name').toLowerCase();
+        // If search query is empty, show all.
+        if (typeof searchQuery === 'undefined' || (typeof name === 'string' && name.indexOf(searchQuery) !== -1)) {
+            $('ul.user-list-view').append(element);
+            itemCount++;
+        }
+    }
+
+    // Update count and show notice.
+    const resultText = (itemCount === 1) ? 'result' : 'results';
+    if (searchQuery) {
+        // Show result text
+        $('#user-list-size').text(itemCount + ' ' + resultText);
+    } else {
+        // Show original text.
+        $('#user-list-size').text($('#user-list-size').data('orig-text'));
+    }
+
+    $('#user-list-search-query').text($('#user-list-search').val());
+    $('#user-list-search-notice').show();
+
+    // Hide list count if no results, and show notice. Otherwise, opposite.
+    if (itemCount > 0) {
+        $('#user-list-size').show();
+        $('#user-list-search-no-results').hide();
+    } else {
+        $('#user-list-size').hide();
+        $('#user-list-search-no-results').show();
+    }
 }
